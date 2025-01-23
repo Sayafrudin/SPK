edc87ece4a4f869b64692e55008a90ab66de99fa
syafrudinhl <syafrudintdm@gmail.com>
2025-01-23 16:24:45 +0700

    fix heading

--- resources/views/excel/index.blade.php
+++ resources/views/excel/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Obat /Upload File Excel') }}
+            {{ __('Obat / Upload File Excel') }}
         </h2>
     </x-slot>
 
@@ -10,7 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="flex items-center justify-center">
-
                         <!-- Author: FormBold Team -->
                         <div class="mx-auto w-full max-w-[550px] bg-white">
                             <form action="" method="POST" class="pb-4 pt-0 px-9" enctype="multipart/form-data">
