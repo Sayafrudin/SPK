8b268771a7ad8dd9edb219b6e86fea3c6033c21e
syafrudinhl <syafrudintdm@gmail.com>
2025-01-26 20:31:07 +0700

    center text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
                         {{ __('Stok Obat Chart') }}
                     </h2>
                     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
