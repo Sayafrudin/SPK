6ed70335cc6909cbc81bc280780552e4f97fb7ae
syafrudinhl <syafrudintdm@gmail.com>
2025-01-26 20:29:59 +0700

    add table, primary key etc

--- app/Models/Alternatif.php
+++ app/Models/Alternatif.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Alternatif extends Model
 {
-    //
+    use HasFactory;
+    // use SoftDeletes;
+
+    protected $table = 'alternatif'; // Nama tabel
+    protected $primaryKey = 'id_alternatif';
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'kode_alternatif',
+        'id_obat',
+        'c1',
+        'c2',
+        'c3',
+        'c4',
+    ];
+  
+    // Definisikan relasi jika diperlukan
+    public function obat()
+    {
+        return $this->belongsTo(Obat::class, 'id_obat');
+    }
 }
